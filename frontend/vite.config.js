46294cb480454e5d7fb0c11dbf784d74e294e981
Abhishek Soni <abhishek.22bcon1471@jecrcu.edu.in>
2024-07-20 11:07:23 +0530

    dist folder

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 5000,
     proxy: {
-      "/api": {
-        target: 'https://movieenthusiast-backend.onrender.com/', // Replace with your deployed backend URL
+      '/api': {
+        target: 'https://movieenthusiast-backend.onrender.com/', 
         changeOrigin: true,
-        secure: true, }
-    }
-  }
-})
+        secure: true,
+      },
+    },
+  },
+});
